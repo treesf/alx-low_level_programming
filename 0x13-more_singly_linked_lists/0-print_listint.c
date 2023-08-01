ff0adf86f5fd6d6b52153417c8d9ea42d7f9a109
treesf <trees@ltaelectronics.com>
2023-08-01 10:31:49 -0700

    ALX C - More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,21 +5,18 @@
   * print_listint - Prints all the elements of a linked list
   * @h: The head of the linked list
   *
-  * Return: The number of nodes of the linked list
+  * Return: the number of nodes
   */
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
+	const listint_t *cursor = h;
+	size_t count = 0;
 
-	if (h != NULL)
+	while (cursor != NULL)
 	{
-		while (h)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			count++;
-		}
+		printf("%d\n", cursor->n);
+		count += 1;
+		cursor = cursor->next;
 	}
-
 	return (count);
 }
